6b901f146e35f2cc13bb65c23eaf20603b194bcd
Tomoki <makatu1999@icloud.com>
2019-06-22 18:52:30 +0900

    [TEST] Test commit from new env

--- task-radio-front/src/views/Home.vue
+++ task-radio-front/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	</div>
 </template>
 
 <script lang="ts">
@@ -10,15 +10,17 @@ import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+	components: {
+		HelloWorld,
+	},
 })
 export default class Home extends Vue {}
 </script>
 <style>
 div.home{
-padding:0% 0% 0% 0%;
-
+	margin:0% 0% 0% 0%;
+	padding:0% 0% 0% 0%;
+	border:0%;
 }
+
 </style>
