4d3810e4728df075a18694a33bd62b04399f8a40
Tomoki <makatu1999@icloud.com>
2019-06-23 00:32:15 +0900

    schocolate

--- task-radio-front/src/App.vue
+++ task-radio-front/src/App.vue
@@ -10,6 +10,11 @@
 
 <style>
 #app {
+  position: absolute;
+  top: 0%;
+  bottom: 0%;
+  left: 0%;
+  right: 0%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
